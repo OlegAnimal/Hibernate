6ad86fe230a1ab7b49c3e061e15078fc49958480
AnimalO <o.loginoff@gmail.com>
2023-03-27 17:57:40 +0300

    Добавил rollback транзакций

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -3,6 +3,7 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
@@ -14,8 +15,9 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
+        Transaction transaction = null;
         try (Session session = Util.getSessionFactory().getCurrentSession()) {
-            session.beginTransaction();
+            transaction = session.beginTransaction();
             String sql = "CREATE TABLE IF NOT EXISTS USERS " +
                     "(id INTEGER NOT NULL AUTO_INCREMENT, " +
                     " name VARCHAR(255), " +
@@ -24,6 +26,7 @@ public class UserDaoHibernateImpl implements UserDao {
             session.createSQLQuery(sql).executeUpdate();
             session.getTransaction().commit();
         } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
             throw e;
         }
 
@@ -31,24 +34,27 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void dropUsersTable() {
+        Transaction transaction = null;
         try (Session session = Util.getSessionFactory().getCurrentSession()) {
-            session.beginTransaction();
+            transaction = session.beginTransaction();
             String sql = "DROP TABLE IF EXISTS USERS";
             session.createSQLQuery(sql).executeUpdate();
             session.getTransaction().commit();
         } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
             throw e;
         }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        Transaction transaction = null;
         try (Session session = Util.getSessionFactory().getCurrentSession()) {
-            session.beginTransaction();
-//            User usr = new User(name, lastName, age);
+            transaction = session.beginTransaction();
             session.save(new User(name, lastName, age));
             session.getTransaction().commit();
         } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
             throw e;
         }
 
@@ -56,35 +62,41 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void removeUserById(long id) {
+        Transaction transaction = null;
         try (Session session = Util.getSessionFactory().getCurrentSession()) {
-            session.beginTransaction();
+            transaction = session.beginTransaction();
             User usr = session.get(User.class, id);
             session.delete(usr);
             session.getTransaction().commit();
         } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
             throw e;
         }
     }
 
     @Override
     public List<User> getAllUsers() {
+        Transaction transaction = null;
         try (Session session = Util.getSessionFactory().getCurrentSession()) {
-            session.beginTransaction();
+            transaction = session.beginTransaction();
             List<User> usr = session.createQuery("from USERS", User.class).getResultList();
             session.getTransaction().commit();
             return usr;
         } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
             throw e;
         }
     }
 
     @Override
     public void cleanUsersTable() {
+        Transaction transaction = null;
         try (Session session = Util.getSessionFactory().getCurrentSession()) {
-            session.beginTransaction();
+            transaction = session.beginTransaction();
             session.createQuery("DELETE from USERS").executeUpdate();
             session.getTransaction().commit();
         } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
             throw e;
         }
 
